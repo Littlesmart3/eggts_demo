9adcbe87e24c12f892a7841f2866de7e1dd51294
范力 <798347235@qq.com>
2021-09-16 09:05:20 +0800

    fix: bug

---
+++ app/service/lee.ts
@@ -0,0 +1,19 @@
+
+import { Service } from 'egg';
+
+
+// const Service = require('egg').Service;
+
+class LeeService extends Service {
+  async getGirl(id) {
+    // 因为没有真实连接数据库，所以模拟数据
+    return {
+      id,
+      name: '小红',
+      age: 18,
+    };
+
+  }
+}
+
+module.exports = LeeService;
